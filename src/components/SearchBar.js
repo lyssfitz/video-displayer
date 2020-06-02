41d5972a0a75b02f6e3ab6fc5cc72f29c6379004
Lyss <lyssfitz@outlook.com>
2020-06-02 11:44:42 +1000

    Add form to SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+	state = { term: '' };
+
+	onInputChange = (event) => {
+		this.setState({ term: event.target.value });
+	};
+
+	onFormSubmit = (event) => {
+		event.preventDefault();
+		//Todo: call callback from parent component.
+	};
 	render() {
-		return <div>SearchBar</div>;
+		return (
+			<div className="search-bar ui segment">
+				<form onSubmit={this.onFormSubmit} className="ui form">
+					<div className="field">
+						<label>Video Search</label>
+						<input type="text" value={this.state.term} onChange={this.onInputChange} />
+					</div>
+				</form>
+			</div>
+		);
 	}
 }
 
